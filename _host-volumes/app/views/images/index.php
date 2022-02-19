fb4286619cb5b626ca52807be70db107ef092085
sotaryoutarou <sotaryoutarou@gmail.com>
2022-02-19 17:03:14 +0900

    make upload form

---
+++ _host-volumes/app/views/images/index.php
@@ -0,0 +1,6 @@
+<form action="images/upload" method="post" enctype="multipart/form-data">
+　<label for="picture">画像 ※pngのみ</label>
+  <input type="file" name="picture" id="picture">
+  <br>
+  <input type="submit" name="botan" value="send">
+</form>
